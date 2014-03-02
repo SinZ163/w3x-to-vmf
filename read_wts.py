b0cd8328b67749163db1d84ed04e5eea3950acdf
RenolY2 <debugyoshi@gmail.com>
2014-03-02 11:47:33 +0100

    Modified mode of operation
    Added exception raising if a { or } is found inside a line, but not at
    the start. STRING is now searched for on a single line, data found
    before STRING will be disregarded. Added sorting by integer so the
    resulting JSON output will have the strings sorted by number instead of
    alphabetically.

--- read_wts.py
+++ read_wts.py
@@ -1,43 +1,72 @@
-#The triggers database
+#The .wts file contains the triggers database
+
 import simplejson
 import os
+
 class ReadWTS:
-    currentString = None
     def __init__(self, filename):
         self.filename = filename
         self.triggers = {}
-        self.readLoop()
+        
+        self.currentString = None
+        
     def readLoop(self):
         status = 0
+        
         with open(self.filename) as f:
-            for line in f:
-                lineSplit = line.split()
-                if self.currentString:
-                    if "{" in lineSplit[0]:
+            for lineNumber, line in enumerate(f):
+                
+                if self.currentString != None:
+                    if line.startswith("{"):
                         self.triggers[self.currentString] = []
                         continue
-                    elif "}" in lineSplit[0]:
+                    
+                    elif line.startswith("}"):
                         self.currentString = None
                         continue
+                    
+                    elif "{" in line or "}" in line:
+                        print "An unhandled case on line {0}, please check it out and report it.".format(lineNumber+1)
+                        print line
+                        raise RuntimeError("{ or } found in line, but not at the start.")
+                    
                     else:
-                        newLine = ""
-                        for word in lineSplit:
-                            if word == "//":
-                                break
-                            else:
-                                newLine = newLine + " " + word
+                        # Two slashes appear to be used for comments, so we will
+                        # only read a line until we hit a double slash.
+                        DoubleSlash = line.find("//")
+                        
+                        newLine = line[0:DoubleSlash]
+                                
                         if newLine != "":
                             self.triggers[self.currentString].append(newLine)
-                else:              
-                    if len(lineSplit) > 0:
-                        if "STRING" in lineSplit[0]:
-                            self.currentString = lineSplit[1]
+                else:
+                    STRINGpos = line.find("STRING")
+                    
+                    if STRINGpos != -1:
+                        line = line[STRINGpos:]
+                        lineSplit = line.split()
+                        
+                        assert lineSplit[0] == "STRING"
+                        
+                        stringID = lineSplit[1]
+                        self.currentString = int(stringID)
+
+def integerSort(key):
+    return int(key[0])
+
+                            
 if __name__ == "__main__":
     filename = "input/war3map_troll.wts"
+    
     info = ReadWTS(filename)
+    info.readLoop()
+    
     try:
         os.makedirs('./output')
     except OSError:
         pass
+    
     with open("output/triggers.json", "w") as f:
-        f.write(simplejson.dumps(info.triggers, sort_keys=True, indent=4 * ' '))
+        f.write(simplejson.dumps(info.triggers, item_sort_key=integerSort, indent=4 * ' '))
+    
+    print ".wts reading finished"
