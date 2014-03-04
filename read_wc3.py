c448a5ef22337f533622e62632ec68bf18c7f995
RenolY2 <debugyoshi@gmail.com>
2014-03-04 11:45:39 +0100

    Added a wc3 map unpacking test

--- read_wc3.py
+++ read_wc3.py
@@ -1,7 +1,7 @@
-#import struct
+import os
 
 from lib.DataReader import DataReader
-from lib.mpyq import MPQArchive, WC3Map_MPQ
+from lib.mpyq import MPQArchive, WC3Map_MPQ, UnsupportedCompressionAlgorithm
 
 # Generating a basic listfile for the map. Basically, we iterate over the 
 # listfile template and check if we can read each file from the archive. 
@@ -14,28 +14,40 @@ def generate_listfile(wc3files, mpq):
     
     for line in filelist:
         try:
-            output = mpq.read_file(line.strip())
+            output = mpq.read_file(line.strip(), raw = False)
             
             if output != None:
                 # The file appears to have been found successful
                 listfile.append((line.strip(), True)) 
+                filename = line.strip().replace("\\", "_")
+                
+                with open("output/mapdata/"+filename, "wb") as temp:
+                    temp.write(output)
+                
             else:
                 # The file doesn't appear to exist, or has a zero size in the archive
                 listfile.append((line.strip(), None)) 
                 
-        except Exception as error:
-            # The file does exist, but there has been some trouble, 
-            # e.g. the file is encrypted or uses an unsupported compression algorithm
+        except UnsupportedCompressionAlgorithm as error:
+            # The file does exist, but uses an unsupported compression algorithm
             listfile.append((line.strip(), False))
             print line.strip(), error
                                                     
     return listfile
 
 filename = "map.w3x"
+#filename = "input/trollsnelves.w3x"
 
 archive = WC3Map_MPQ(filename, listfile=False)
 
 print "Trying to generate a listfile"
+
+try:
+    os.mkdir("output/mapdata")
+except Exception as error:
+    print error
+    print "Continuing"
+    
 
 listfile = generate_listfile("lib/wc3Files.txt", archive)
 
@@ -47,8 +59,10 @@ listfile_file.close()
 listfile_file = open("output/listfile.txt", "a")
 
 for entry in listfile:
-    if entry[1] in [True, False]:
-        listfile_file.write(entry[0]+"\n")
+    if entry[1] == True:
+        listfile_file.write(entry[0]+" successfully read\n")
+    elif entry[1] == False:
+        listfile_file.write(entry[0]+" found, but unsupported compression algorithm\n")
 
 # Done! Please note that this is not always the full listfile.
 # Custom maps are likely to have their own listfiles which are located
