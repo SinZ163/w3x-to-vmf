63516db81ec6c9348ec1b060ffa87a2d9f27818d
RenolY2 <debugyoshi@gmail.com>
2014-03-12 13:17:48 +0100

    One step towards a true w3x reader

---
+++ WC3MapObject.py
@@ -0,0 +1,9 @@
+
+
+# This object will provide an interface for accessing the 
+# various data of a wc3 map. It will unpack the MPQ data from
+# the map, and it will automatically use the read_<insert file format>
+# scripts to extract the information from the files.
+class WC3Map():
+    def __init__(self):
+        pass
