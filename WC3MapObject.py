92f87b13d8f43496002e9510c26c64139e1db8c8
RenolY2 <debugyoshi@gmail.com>
2014-07-20 16:27:57 +0200

    Correct imports, add test that loads many maps from a directory
    Changed imports to reflect the recent changes in naming for the
    read filetype scripts
    
    Added a test that loads WC3 maps from a directory and tries to create a
    listfile for them. This allows us to more easily detect oddities in maps,
    e.g. unsupported compression types or issues caused by decompression.

--- WC3MapObject.py
+++ WC3MapObject.py
@@ -1,14 +1,16 @@
 import os
+import traceback
+import simplejson
 
 from lib.mpyq import WC3Map_MPQ, UnsupportedCompressionAlgorithm
 
-from lib.ReadFiletype_Scripts.read_doo import ReadDoodad
-from lib.ReadFiletype_Scripts.read_object import ObjectReader
-from lib.ReadFiletype_Scripts.read_slk import ReadSLK
-from lib.ReadFiletype_Scripts.read_w3e import ReadW3E
-from lib.ReadFiletype_Scripts.read_w3i import WC3Info
-from lib.ReadFiletype_Scripts.read_wpm import ReadPathFile
-from lib.ReadFiletype_Scripts.read_wts import ReadWTS
+from lib.ReadFiletype.read_doo import read_doodad
+from lib.ReadFiletype.read_slk import read_SLK
+from lib.ReadFiletype.read_w3e import read_W3E
+from lib.ReadFiletype.read_w3i import read_W3I
+from lib.ReadFiletype.read_wpm import read_WPM
+from lib.ReadFiletype.read_wts import read_WTS
+from lib.ReadFiletype.read_object import read_object
  
 
 # This object will provide an interface for accessing the 
@@ -16,12 +18,9 @@ from lib.ReadFiletype_Scripts.read_wts import ReadWTS
 # the map, and it will automatically use the read_<insert file format>
 # scripts to extract the information from the files.
 class WC3Map():
-    def __init__(self, filehandle, useListfile = False):
-        self.mpq = WC3Map_MPQ(filehandle, useListfile)
+    def __init__(self, filehandle, useListfile = False, strict = False, forceV1 = False):
+        self.mpq = WC3Map_MPQ(filehandle, useListfile, strict, forceV1)
         self.listfile = []
-        
-        self.createListfile()
-        
         
     def createListfile(self):
         standard_list = open(os.path.join("lib", "wc3Files.txt"), "r")
@@ -31,10 +30,12 @@ class WC3Map():
             
             try:
                 file = self.mpq.read_file(line)
-            except UnsupportedCompressionAlgorithm:
-                print line, "has unsupported compression"
-            except:
-                pass
+            except UnsupportedCompressionAlgorithm as error:
+                print line, "has unsupported compression: ",error.name, error.used_algorithms
+                self.listfile.append(file)
+            except Exception as error:
+                print line, "caused an exception:", str(error)
+                traceback.print_exc()
             else:
                 if file != None:
                     print line, "found"
@@ -42,5 +43,21 @@ class WC3Map():
         
 
 if __name__ == "__main__":
-    with open("input/trollsnelves.w3x", "rb") as f:
-        mymap = WC3Map(f)
+    WCdirectory = "C:\\Games\\Warcraft III\\Maps\\CustomMaps"
+    files = os.listdir(WCdirectory)
+
+    for filename in  files:
+        path = os.path.join(WCdirectory, filename)
+        if os.path.isdir(path):
+            continue
+        
+        print "\n======================="
+        print path
+        
+        with open(path, "rb") as f:
+            try:
+                mymap = WC3Map(f, forceV1 = True)
+                mymap.createListfile()
+            except OverflowError:
+                print path, "CAUSES OVERFLOWERROR"
+                traceback.print_exc()
